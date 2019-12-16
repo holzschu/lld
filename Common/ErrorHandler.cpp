7b8318293e39661e6a7ff24f8f714b7b37126322
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2019-12-16 22:22:36 +0100

    Changed exit to ios_exit to avoid crashing the program

--- Common/ErrorHandler.cpp
+++ Common/ErrorHandler.cpp
@@ -22,6 +22,13 @@
 #include <unistd.h>
 #endif
 
+#ifdef __APPLE__
+#include <TargetConditionals.h>
+#if (TARGET_OS_IPHONE || TARGET_IPHONE_SIMULATOR)
+#include "ios_error.h"
+#endif
+#endif
+
 using namespace llvm;
 using namespace lld;
 
--- tools/lld/lld.cpp
+++ tools/lld/lld.cpp
@@ -38,6 +38,13 @@
 #include "llvm/Support/Path.h"
 #include <cstdlib>
 
+#ifdef __APPLE__
+#include <TargetConditionals.h>
+#if (TARGET_OS_IPHONE || TARGET_IPHONE_SIMULATOR)
+#include "ios_error.h"
+#endif
+#endif
+
 using namespace lld;
 using namespace llvm;
 using namespace llvm::sys;
